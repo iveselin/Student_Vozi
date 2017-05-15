97a493238074baee3138f86e868534701d25e4db
Ivan <ivanveselin@hotmail.com>
2017-05-15 12:45:28 +0200

    Added basic activities, KV3

---
+++ app/src/main/java/hr/ferit/iveselin/studentvozi/AddRequestActivity.java
@@ -0,0 +1,15 @@
+package hr.ferit.iveselin.studentvozi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_request);
+        //implementirati provjeru što se treba dodati, ponuda ili potražnja te ovisno o tome ispuniti ekran
+
+    }
+}
--- app/src/main/java/hr/ferit/iveselin/studentvozi/MainActivity.java
+++ app/src/main/java/hr/ferit/iveselin/studentvozi/MainActivity.java
@@ -2,12 +2,35 @@ package hr.ferit.iveselin.studentvozi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    Button bAddRide, bFindRide;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setUI();
+        //check if loged in, if not redirect to loginActivity
+    }
+
+    private void setUI() {
+        this.bAddRide= (Button) findViewById(R.id.bAddRide);
+        this.bFindRide= (Button) findViewById(R.id.bFindRide);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bAddRide:
+
+                break;
+            case R.id.bFindRide:
+
+                break;
+        }
     }
 }
