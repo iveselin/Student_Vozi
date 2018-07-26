441dfa1bc36db76a7f9f02c9fd59fd53d2df3302
Ivan Veselin <ivanveselin@hotmail.com>
2018-07-26 10:14:55 +0200

    updated libs and added butterknife

--- app/src/main/java/hr/ferit/iveselin/studentvozi/AddRequestActivity.java
+++ app/src/main/java/hr/ferit/iveselin/studentvozi/AddRequestActivity.java
@@ -7,28 +7,15 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
 
-public class AddRequestActivity extends AppCompatActivity implements View.OnClickListener{
-    Button bSubmit;
-    DatePicker dpSetDate;
-    TimePicker tpSetTime;
+public class AddRequestActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_request);
-        this.setUI();
-        //implementirati provjeru što se treba dodati, ponuda ili potražnja te ovisno o tome ispuniti ekran
+
 
     }
 
-    private void setUI() {
-        this.bSubmit= (Button) findViewById(R.id.bSubmit);
-        this.dpSetDate= (DatePicker) findViewById(R.id.dpDateSelect);
 
-        //treba sa fragmentima, bit će zajebano
-    }
-
-    @Override
-    public void onClick(View v) {
-
-    }
 }
--- app/src/main/java/hr/ferit/iveselin/studentvozi/MainActivity.java
+++ app/src/main/java/hr/ferit/iveselin/studentvozi/MainActivity.java
@@ -7,37 +7,15 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
-    Button bAddRide, bFindRide;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.setUI();
-        //check if loged in, if not redirect to loginActivity
-    }
-
-    private void setUI() {
-        this.bAddRide= (Button) findViewById(R.id.bAddRide);
-        this.bFindRide= (Button) findViewById(R.id.bFindRide);
-        bAddRide.setOnClickListener(this);
-        bFindRide.setOnClickListener(this);
 
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.bAddRide:
-                //temporary for testing map activity and fragment
-                Intent sampleIntent=new Intent();
-                sampleIntent.setClass(getApplicationContext(),LocationActivity.class);
-                this.startActivity(sampleIntent);
-                break;
-            case R.id.bFindRide:
 
-                break;
-        }
-    }
 }
