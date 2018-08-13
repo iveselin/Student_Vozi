228ed87eebccd4918d2854f398e983b47913230f
Ivan Veselin <ivanveselin@hotmail.com>
2018-08-13 22:17:51 +0200

    implemented permissions check and map location

--- app/src/main/java/hr/ferit/iveselin/studentvozi/LocationActivity.java
+++ app/src/main/java/hr/ferit/iveselin/studentvozi/LocationActivity.java
@@ -6,13 +6,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -25,7 +36,15 @@ public class LocationActivity extends BaseActivity implements OnMapReadyCallback
 
     private static final String TAG = "LocationActivity";
 
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    public static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    public static final int LOCATION_PERMISSION_REQUEST_CODE = 1111;
+    public static final float DEFAULT_ZOOM = 15f;
+
     private GoogleMap map;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    private boolean permissionsGranted = false;
 
 
     public static Intent getLaunchIntent(Context fromContext) {
@@ -40,14 +59,18 @@ public class LocationActivity extends BaseActivity implements OnMapReadyCallback
         setUi();
     }
 
+    private void setUi() {
+        ButterKnife.bind(this);
+    }
 
-    // TODO: 8.8.2018. create two input fields, action bar and so on 
+
+    // TODO: 8.8.2018. create two input fields, action bar and so on
 
     @Override
     protected void onStart() {
         super.onStart();
         checkLogin();
-        checkPermissions();
+        checkLocationPermissions();
     }
 
 
@@ -60,18 +83,83 @@ public class LocationActivity extends BaseActivity implements OnMapReadyCallback
         }
     }
 
-    private void checkPermissions() {
+    private void checkLocationPermissions() {
 
+        if ((ContextCompat.checkSelfPermission(getApplicationContext(), COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+             && (ContextCompat.checkSelfPermission(getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
+                permissionsGranted = true;
+                initMap();
+                return;
+            }
+
+
+        String[] permissions = {FINE_LOCATION, COURSE_LOCATION};
+        ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
     }
 
-    private void setUi() {
-        ButterKnife.bind(this);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsGranted = false;
+
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            permissionsGranted = false;
+                            return;
+                        }
+                    }
+                    permissionsGranted = true;
+                    initMap();
+                }
+            }
+        }
+    }
+
+    private void initMap() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    private void getDeviceLocation() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            if (permissionsGranted) {
+                Task location = fusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Location currentLocation = (Location) task.getResult();
+
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom) {
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+
+        if (permissionsGranted) {
+            getDeviceLocation();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            map.setMyLocationEnabled(true);
+            map.getUiSettings().setZoomControlsEnabled(true);
+        }
     }
 }
