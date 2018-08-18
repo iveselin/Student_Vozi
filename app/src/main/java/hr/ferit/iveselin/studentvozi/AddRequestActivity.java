92aa90cfb1990f5461d121d9478644d4f6143f4b
Ivan Veselin <ivanveselin@hotmail.com>
2018-08-18 14:04:46 +0200

    added place picking from map

--- app/src/main/java/hr/ferit/iveselin/studentvozi/AddRequestActivity.java
+++ app/src/main/java/hr/ferit/iveselin/studentvozi/AddRequestActivity.java
@@ -6,12 +6,10 @@ import android.content.Context;
 import android.content.Intent;
 
 import android.os.Bundle;
-import android.support.annotation.BinderThread;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.CursorAdapter;
 import android.widget.DatePicker;
 import android.widget.NumberPicker;
 import android.widget.TimePicker;
@@ -32,15 +30,27 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import hr.ferit.iveselin.studentvozi.base.BaseActivity;
+import hr.ferit.iveselin.studentvozi.base.PlaceAutocompleteAdapter;
+import hr.ferit.iveselin.studentvozi.utils.LoginUtils;
 
 public class AddRequestActivity extends BaseActivity implements DatePickerDialog.OnDateSetListener,
         TimePickerDialog.OnTimeSetListener,
         GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "AddRequestActivity";
+    public static final int KEY_ADDRESS_FROM_MAP_DEPARTURE = 111;
+    public static final int KEY_ADDRESS_FROM_MAP_DESTINATION = 222;
+    private static final String KEY_PICKED_ADDRESS = "picked_address";
+
 
     public static Intent getLaunchIntent(Context fromContext) {
         return new Intent(fromContext, AddRequestActivity.class);
+    }
+
+    public static Intent getResultsIntent(String pickedAddress) {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(KEY_PICKED_ADDRESS, pickedAddress);
+        return resultIntent;
     }
 
     private static final int KEY_LOCATION_REQUEST = 111;
@@ -54,18 +64,10 @@ public class AddRequestActivity extends BaseActivity implements DatePickerDialog
 
     @BindView(R.id.number_input_value)
     NumberPicker numberPicker;
-
     @BindView(R.id.add_date)
     Button dateInput;
-
     @BindView(R.id.add_time)
     Button timeInput;
-
-    /*@BindView(R.id.departure_location_input)
-    AutoCompleteTextView departureInput;
-
-    @BindView(R.id.destination_location_input)
-    AutoCompleteTextView destinationLocation;*/
 
     AutoCompleteTextView departureInput;
     AutoCompleteTextView destinationInput;
@@ -104,17 +106,14 @@ public class AddRequestActivity extends BaseActivity implements DatePickerDialog
     @Override
     protected void onStart() {
         super.onStart();
-        checkLogin();
-    }
 
-    private void checkLogin() {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user == null) {
+        if (!LoginUtils.checkLogin()) {
             Toast.makeText(getApplicationContext(), "You have to login", Toast.LENGTH_SHORT).show();
             startActivity(LoginActivity.getLaunchIntent(this));
             finish();
         }
     }
+
 
     @OnClick(R.id.add_date)
     void onAddDateClicked() {
@@ -137,10 +136,17 @@ public class AddRequestActivity extends BaseActivity implements DatePickerDialog
         timePicker.show();
     }
 
-    /*@OnClick(R.id.add_location)
-    void onAddLocationClicked() {
-        startActivityForResult(LocationActivity.getLaunchIntent(this), KEY_LOCATION_REQUEST);
-    }*/
+    @OnClick(R.id.map_icon_departure)
+    void onMapDepartureClicked() {
+        String typedAddress = departureInput.getText().toString();
+        startActivityForResult(LocationActivity.getLaunchIntent(this, typedAddress), KEY_ADDRESS_FROM_MAP_DEPARTURE);
+    }
+
+    @OnClick(R.id.map_icon_destination)
+    void onMapDestinationClicked() {
+        String typedAddress = destinationInput.getText().toString();
+        startActivityForResult(LocationActivity.getLaunchIntent(this, typedAddress), KEY_ADDRESS_FROM_MAP_DESTINATION);
+    }
 
     @OnClick(R.id.submit_request)
     void onSubmitClicked() {
@@ -169,9 +175,16 @@ public class AddRequestActivity extends BaseActivity implements DatePickerDialog
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         if (resultCode == RESULT_OK) {
-            if (requestCode == KEY_LOCATION_REQUEST) {
-                // TODO: 8.8.2018. extract locations from result data
+            String pickedAddress = data.getStringExtra(KEY_PICKED_ADDRESS);
+            switch (requestCode) {
+                case KEY_ADDRESS_FROM_MAP_DEPARTURE:
+                    departureInput.setText(pickedAddress);
+                    break;
+                case KEY_ADDRESS_FROM_MAP_DESTINATION:
+                    destinationInput.setText(data.getStringExtra(pickedAddress));
+                    break;
             }
         }
     }
